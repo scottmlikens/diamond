a7c0dcac31af00cb454d5d6f0f8c1b9fbc2da31e
Scott M. Likens <scott@likens.us>
2014-01-09 00:02:25 -0800

    bump psutil to 1.2.1

--- resources/install.rb
+++ resources/install.rb
@@ -3,7 +3,7 @@ actions :git, :tarball
 attribute :prefix, :kind_of => String, :default => "/opt/diamond"
 attribute :git_repository_uri, :kind_of => String, :default => "https://github.com/BrightcoveOS/Diamond.git"
 attribute :git_reference, :kind_of => String, :default => "master"
-attribute :required_python_packages, :kind_of => Hash, :default => { "configobj" => "4.7.2","psutil" => "0.6.1"}
+attribute :required_python_packages, :kind_of => Hash, :default => { "configobj" => "4.7.2","psutil" => "1.2.1"}
 attribute :cookbook, :kind_of => String, :default => "diamond"
 attribute :tarball_path, :kind_of => String, :default => "https://github.com/BrightcoveOS/Diamond/archive/master.tar.gz"
 attribute :tarball_extract_fldr, :kind_of => String, :default => "Diamond-master"
