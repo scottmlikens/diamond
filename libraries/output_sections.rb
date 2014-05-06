31dd9b865df43e690e52eaf03de6796da5105be0
Walter  Huf <walter.huf@corvisa.com>
2014-05-06 17:19:57 -0500

    Sorts the template keys before rendering

--- libraries/output_sections.rb
+++ libraries/output_sections.rb
@@ -3,13 +3,15 @@ module Chef::Recipe::Diamond
     config = config.to_hash  # enables .reject
     lines = []
     mydata = config.reject {|k,v| v.respond_to?('each_pair')}
-    mydata.each_pair do |k,v|
+    mydata.keys.sort_by{|k|k.to_s}.each do |k|
+      v = mydata[k]
       lines << k.to_s + ' = ' + v.to_s
     end
     lines << ''
     depth = depth + 1
     subsections = config.reject {|k,v| not v.respond_to?('each_pair')}
-    subsections.each_pair do |k,v|
+    subsections.keys.sort_by{|k|k.to_s}.each do |k|
+      v = subsections[k]
       lines << ('[' * depth) + k.to_s + (']' * depth)
       lines = lines.concat(output_sections(v, depth))
     end
