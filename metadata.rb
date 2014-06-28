c0a6093e0b7e9b16b06b6d31d117931ab4cc00a2
Scott M. Likens <scott@likens.us>
2014-06-27 19:48:40 -0700

    bump it to 3.0 as this is a pretty big change

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "scott at likens dot us"
 license          "Apache 2.0"
 description      "Installs/Configures diamond"
 
-version          "0.2.7"
+version          "0.3.0"
 
 supports "ubuntu"
 
