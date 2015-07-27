df8c20a83b16c1511e54a8de8440de5c1591abd3
Scott M. Likens <scott@likens.us>
2015-07-26 18:04:53 -0700

    Fixes #17
    Signed-off-by: Scott M. Likens <scott@likens.us>

--- resources/install.rb
+++ resources/install.rb
@@ -1,11 +1,11 @@
 actions :git, :tarball
 
 attribute :prefix, :kind_of => String, :default => "/opt/diamond"
-attribute :git_repository_uri, :kind_of => String, :default => "https://github.com/BrightcoveOS/Diamond.git"
+attribute :git_repository_uri, :kind_of => String, :default => "https://github.com/python-diamond/Diamond.git"
 attribute :git_reference, :kind_of => String, :default => "master"
 attribute :required_python_packages, :kind_of => Hash, :default => { "configobj" => "4.7.2","psutil" => "1.2.1"}
 attribute :cookbook, :kind_of => String, :default => "diamond"
-attribute :tarball_path, :kind_of => String, :default => "https://github.com/BrightcoveOS/Diamond/archive/master.tar.gz"
+attribute :tarball_path, :kind_of => String, :default => "https://github.com/python-diamond/Diamond/archive/master.tar.gz"
 attribute :tarball_extract_fldr, :kind_of => String, :default => "Diamond-master"
 
 def initialize(*args)
@@ -13,4 +13,3 @@ def initialize(*args)
   @action = :create
   @run_context.include_recipe ["build-essential","git","python","python::pip","python::virtualenv","runit::default"]
 end
-
